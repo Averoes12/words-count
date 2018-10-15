326a8b68279df4cc7c473a63c8a0d0723b8b1845
averoes12 <hz.ir.daff@gmail.com>
2018-10-15 16:05:09 +0700

    word-count

---
+++ word-count.js
@@ -0,0 +1,11 @@
+function wordcount(string) {
+
+    return string.split(' ').length;
+
+    
+}
+
+console.log(wordcount('I have a dream'))
+console.log(wordcount('Never eat shredded wheat or cake'))
+console.log(wordcount('I'))
+console.log(wordcount('I believe I can code'))
